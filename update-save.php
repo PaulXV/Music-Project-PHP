0b6353e8d5559b12513017eb07140961247b7c95
paolo.brugnola <113890106+PaulXV@users.noreply.github.com>
2023-12-23 18:25:25 +0100

    update final part
    FUNZIONA FINALMENTE

--- update-save.php
+++ update-save.php
@@ -1,97 +1,91 @@
 <?php
-    include 'functions.php';
+include 'functions.php';
 
-    $idCanzone = $_GET["idCanzone"];
-    $newTitle = $_GET["title"];
-    $newBand = $_GET["band"];
-    $newGenere = $_GET["genere"];
-    $randomIdBand = "";
-    $randomIdGenere="";
+$idCanzone = $_GET["idCanzone"];
+$newTitle = $_GET["title"];
+$newBand = $_GET["band"];
+$newGenere = $_GET["genere"];
+$randomIdBand = "";
+$randomIdGenere="";
 
-    $rawCanzoni=file_get_contents('canzoni.json');
-    $recordsCanzoni=json_decode($rawCanzoni,true);
+$rawCanzoni=file_get_contents('canzoni.json');
+$recordsCanzoni=json_decode($rawCanzoni);
 
-    $rawBand=file_get_contents('band.json');
-    $recordsBand=json_decode($rawBand, true);
+$rawBand=file_get_contents('band.json');
+$recordsBand=json_decode($rawBand, true);
 
-    $rawGenere=file_get_contents('genere.json');
-    $recordsGeneri=json_decode($rawGenere,true);
+$rawGenere=file_get_contents('genere.json');
+$recordsGeneri=json_decode($rawGenere,true);
 
-    foreach($recordsCanzoni as $canzone){
-        if ($canzone['idCanzone']==$idCanzone){
-            $canzone['titolo'] = $newTitle;
+foreach($recordsCanzoni as $canzone){
+    if ($canzone->idCanzone==$idCanzone){
+        $canzone->titolo = $newTitle;
 
-            /*
-             * controllo se è cambiato il nome della band
-             * se il nome è cambiato controllo che non ci sia già
-             * se c'è già prendo l'id
-             * se non c'è do alla nuova band un nuovo id
-            */
-            foreach($recordsBand as $band){
-                //se il nome è diverso ma l'id è lo stesso allora modifico
-                if($band["idBand"] == $canzone["idBand"]){
-                    if($band["nomeBand"] != $newBand){
-                        $idB = getBandByName($newBand,);
-                        if($idB == ""){
-                            do{                                     //creazione di un nuovo id  tramite numero random
-                                $randomIdBand = rand();
-                            }while(existBand($randomIdBand)==true); // mi controlla che l'id non esista gia'
+        /*
+         * controllo se è cambiato il nome della band
+         * se il nome è cambiato controllo che non ci sia già
+         * se c'è già prendo l'id
+         * se non c'è do alla nuova band un nuovo id
+        */
+        foreach($recordsBand as $band){
+            //se il nome è diverso ma l'id è lo stesso allora modifico
+            if($band["idBand"] == $canzone->idBand){
+                if($band["nomeBand"] != $newBand){
+                    $idB = getBandByName($newBand);
+                    if($idB == ""){
+                        do{                                     //creazione di un nuovo id  tramite numero random
+                            $randomIdBand = rand();
+                        }while(existBand($randomIdBand)==true); // mi controlla che l'id non esista gia'
 
-                            //sta cazzo di assegnazione non funziona e non so perché
-                            $canzone["idBand"] = $idB; //modifico il nuovo id al file delle canzoni
-                            echo $canzone["idBand"];
-                            putBandNewData($randomIdBand, $newBand, $recordsBand);
-                        }else{
-                            //sta cazzo di assegnazione non funziona e non so perché
-                            $canzone["idBand"] = $idB; //modifico il nuovo id al file delle canzoni
-                            echo $canzone["idBand"];
-                            putBandNewData($idB, $newBand, $recordsBand);
-                        }
-                        break;
+                        //sta cazzo di assegnazione non funziona e non so perché
+                        $canzone->idBand = $idB; //modifico il nuovo id al file delle canzoni
+                        putBandNewData($randomIdBand, $newBand, $recordsBand);
+                    }else{
+                        //sta cazzo di assegnazione non funziona e non so perché
+                        $canzone->idBand = $idB; //modifico il nuovo id al file delle canzoni
+                        putBandNewData($idB, $newBand, $recordsBand);
                     }
+                    break;
                 }
             }
-
-            $rawCanzoni=json_encode($recordsCanzoni);
-            file_put_contents('canzoni.json', $rawCanzoni);
-
-            /*
-             * controllo se è cambiato il genere
-             * se il genere è cambiato controllo che non ci sia già
-             * se c'è già prendo l'id
-             * se non c'è do al nuovo genere un nuovo id
-            */
-            foreach($recordsGeneri as $genere){
-                //se il nome è diverso ma l'id è lo stesso allora modifico
-                if($genere["idGenere"]==$canzone["idGenere"]){
-                    if($genere["nomeGenere"] != $newGenere){
-                        $idGen = getGenereByName($newGenere);
-                        if($idGen == ""){
-                            do{                                     //creazione di un nuovo id  tramite numero random
-                                $randomIdGenere = rand();
-                            }while(existGenere($randomIdGenere)==true); // mi controlla che l'id non esista gia'
-
-                            //sta cazzo di assegnazione non funziona e non so perché
-                            $canzone["idGenere"] = $idGen; //modifico il nuovo id al file delle canzoni
-                            echo $canzone["idGenere"];
-                            putGenereNewData($randomIdGenere, $newGenere, $recordsGeneri);
-                        }else{
-                            //sta cazzo di assegnazione non funziona e non so perché
-                            $canzone["idGenere"] = $idGen; //modifico il nuovo id al file delle canzoni
-                            echo $canzone["idGenere"];
-                            putGenereNewData($idGen, $newGenere,$recordsGeneri);
-                        }
-                        $rawCanzoni=json_encode($recordsCanzoni);
-                        file_put_contents('canzoni.json', $rawCanzoni);
-                        break;
-                    }
-                }
-            }
-            $rawCanzoni=json_encode($recordsCanzoni);
-            file_put_contents('canzoni.json', $rawCanzoni);
         }
+
+        $rawCanzoni=json_encode($recordsCanzoni);
+        file_put_contents('canzoni.json', $rawCanzoni);
+
+        /*
+         * controllo se è cambiato il genere
+         * se il genere è cambiato controllo che non ci sia già
+         * se c'è già prendo l'id
+         * se non c'è do al nuovo genere un nuovo id
+        */
+        foreach($recordsGeneri as $genere){
+            //se il nome è diverso ma l'id è lo stesso allora modifico
+            if($genere["idGenere"]==$canzone->idGenere){
+                if($genere["nomeGenere"] != $newGenere){
+                    $idGen = getGenereByName($newGenere);
+                    if($idGen == ""){
+                        do{                                     //creazione di un nuovo id  tramite numero random
+                            $randomIdGenere = rand();
+                        }while(existGenere($randomIdGenere)==true); // mi controlla che l'id non esista gia'
+
+                        //sta cazzo di assegnazione non funziona e non so perché
+                        $canzone->idGenere = $idGen; //modifico il nuovo id al file delle canzoni
+                        putGenereNewData($randomIdGenere, $newGenere, $recordsGeneri);
+                    }else{
+                        //sta cazzo di assegnazione non funziona e non so perché
+                        $canzone->idGenere = $idGen; //modifico il nuovo id al file delle canzoni
+                        putGenereNewData($idGen, $newGenere,$recordsGeneri);
+                    }
+                    $rawCanzoni=json_encode($recordsCanzoni);
+                    file_put_contents('canzoni.json', $rawCanzoni);
+                    break;
+                }
+            }
+        }
+        $rawCanzoni=json_encode($recordsCanzoni);
+        file_put_contents('canzoni.json', $rawCanzoni);
     }
+}
 
-    var_dump($recordsCanzoni);
-
-    //header('Location: index.php');
+header('Location: index.php');
